0c4f2097d25c099ecc29b23b4790756b54a988ae
Georgi Tashkov <gtashkov94@gmail.com>
2021-06-09 14:14:46 +0300

    cleaned up junk code

--- main.go
+++ main.go
@@ -11,7 +11,25 @@ import (
 	"gorm.io/gorm"
 )
 
-//Prints out a list of all the users
+const connStr string = "host=localhost user=postgres password=Weight!123 dbname=weight_loss-db port=5432 sslmode=disable"
+
+func main() {
+
+	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.AutoMigrate(&models.User{})
+	db.AutoMigrate(&models.Weight_log{})
+
+	http.HandleFunc("/users", ListUsers)
+	http.HandleFunc("/userWeight", ListUserWeightLogs)
+	http.ListenAndServe(":3000", nil)
+
+}
+
+// ListUsers: Prints out a list of all the users
 func ListUsers(w http.ResponseWriter, r *http.Request) {
 	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
 	if err != nil {
@@ -32,20 +50,7 @@ func ListUsers(w http.ResponseWriter, r *http.Request) {
 
 }
 
-//To do Must have a function to ask for daily weight log
-func AskWeightLog(which_user int64, weight_log int64) {
-	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	db.Create(&models.Weight_log{
-		Weight_log: weight_log,
-		Userid:     which_user})
-}
-
-//Lists all the weight logs of the specified user
-//To do The userID mus not be hardcoded!
+//ListUserWeightLogs: Prints out a list of all weightlogs of specific user
 func ListUserWeightLogs(w http.ResponseWriter, r *http.Request) {
 	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
 	if err != nil {
@@ -63,58 +68,4 @@ func ListUserWeightLogs(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(weightLogs)
 	fmt.Println(weightLogs)
-
-}
-
-// The function finds the average in an array
-// May be use it inside AverageWeight?
-func SumAverage(array []int) int {
-	sum := 0
-	for _, v := range array {
-		sum += v
-	}
-	result := sum / len(array)
-	return result
-}
-
-//To do Must have a function for calculating the average weight loss ot gain
-
-func AverageWeight(whichUserID int64) {
-	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var allWeightLogs []int
-	err = db.Where("userid = ?", whichUserID).Find(&allWeightLogs).Error
-	if err != nil {
-		log.Println(err)
-		return
-	}
-}
-
-//To do Must have a function for cheering up on consecutive daily logs
-
-//To do Must have a function for cheering up on weight loss
-
-//To do Must have a function for warning if loosing weight too fast
-
-//To do Must have a function for warning if gaining weight instead of loosing
-
-const connStr string = "host=localhost user=postgres password=Weight!123 dbname=weight_loss-db port=5432 sslmode=disable"
-
-func main() {
-
-	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	db.AutoMigrate(&models.User{})
-	db.AutoMigrate(&models.Weight_log{})
-
-	http.HandleFunc("/users", ListUsers)
-	http.HandleFunc("/userWeight", ListUserWeightLogs)
-	http.ListenAndServe(":3000", nil)
-
 }
